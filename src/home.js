ad4d6ac1a583fa040c90282a6f18507a85d772d2
Norman Lew <normanlew@users.noreply.github.com>
2026-05-20 13:47:20 -0700

    Use restaurant image as background image with div on homepage
    Set the restaurant image as a background image within the
    content div and shrank the size of the picture.

--- src/home.js
+++ src/home.js
@@ -1,3 +1,4 @@
+
 import restaurant_img from "./restaurant_interior.jpg"
 
 export function loadHome() {
@@ -14,9 +15,14 @@ export function loadHome() {
     welcome_sub_message.innerHTML = "Inside my restaurant page, you will learn about my restaurant and the many dishes we cook up!";
 
     const restaurant_picture = document.createElement("img");
-    restaurant_picture.src = restaurant_img
+    restaurant_picture.src = restaurant_img;
+    restaurant_picture.id = "restaurant_pic";
+
+    // const restaurant_picture_div = document.createElement("div");
+    // restaurant_picture_div.id = "restaurant_pic";
 
     content.appendChild(welcome_message);
     content.appendChild(welcome_sub_message);
     content.appendChild(restaurant_picture);
+    // content.appendChild(restaurant_picture_div);
 }
