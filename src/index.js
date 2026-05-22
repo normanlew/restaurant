53aacb777ef7042468715c2e72297422a8fad19d
Norman Lew <normanlew@users.noreply.github.com>
2026-05-22 11:56:21 -0700

    Add event listeners to navigation buttons
    Added event listeners to the home and menu tabs and tested
    their functionality.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import "./styles.css";
 import {loadHome} from "./home.js";
 import {loadMenu} from "./menu.js";
-// loadHome();
-loadMenu();
+loadHome();
+// loadMenu();
+
+const home_button = document.querySelector("#home");
+
+const content_div = document.querySelector("#content");
+
+home_button.addEventListener("click", (e) => {
+    content_div.replaceChildren();
+    loadHome();
+});
+
+const menu_button = document.querySelector("#menu");
+
+menu_button.addEventListener("click", (e) => {
+    content_div.replaceChildren();
+    loadMenu();
+})
 
 
-console.log("test");
+// console.log("test");
